19a2f430ecccfd6d26b5c2cc65b3681c370e336e
Aderson <1java2scrip3@gmail.com>
2023-06-28 16:26:53 -0500

    Refactor App Component

--- src/App.vue
+++ src/App.vue
@@ -1,121 +1,103 @@
-<script>
-import Alert from './components/Alert.vue';
-import Navbar from './components/Navbar.vue';
-import TodoForm from './components/TodoForm.vue';
-import Todos from './components/Todos.vue';
-import Spinner from './components/Spinner.vue';
-import EditTodoForm from './components/EditTodoForm.vue';
-//import api from './api.js'
-export default {
-  components: {
-    Alert,
-    Navbar,
-    TodoForm,
-    Todos,
-    EditTodoForm,
-    Spinner
-},
-  data() {
-    return {
-      todos: [],
-      alert: {
-        show: false,
-        message: "",
-        variant: "warning"
-      },
-      isLoading: false,
-      isPostingTodo: false,
-      editTodoForm: {
-        show: false,
-        todo: {
-          id: 0,
-          tittle: ""
-        }
-      }
+<script setup>
+  import Alert from './components/Alert.vue';
+  import Navbar from './components/Navbar.vue';
+  import TodoForm from './components/TodoForm.vue';
+  import Todos from './components/Todos.vue';
+  import Spinner from './components/Spinner.vue';
+  import EditTodoForm from './components/EditTodoForm.vue';
+  import { ref, reactive } from 'vue';
+  //import api from './api.js'
+  const todos = ref([]);
+  const isLoading = ref(false);
+  const isPostingTodo = ref(false);
+  const alert = reactive({
+    show: false,
+    message: "",
+    variant: "warning"
+  });
+  const editTodoForm = reactive({
+    show: false,
+    todo: {
+      id: 0,
+      tittle: ""
     }
-  },
-  created() {
-    //console.log(import.meta.env.VITE_API_URL);
-    this.fetchTodos();
-  },
-  methods: {
-    showAlert(message, type = "danger") {
-      this.alert.message = message;
-      this.alert.variant = type;
-      this.alert.show = true;
-    },
-    fetchTodos() {
-      this.isLoading = true;
-      fetch('/api/todos/')
-      .then(resp => resp.json())
-      .then(data => {
-        this.isLoading = false;
-        this.todos = data;
-      })
-      .catch(err => this.showAlert('Failing loading Todos...'));
-      //console.log(todos);
-    },
-    addTodo(tittle){
-      if (tittle !== '') {
-        this.alert.show = false;
-        this.isPostingTodo = true;
-        const todo = { tittle };
-        fetch('/api/todos/', {
-            method: 'POST',
-            headers: {
-              'Content-type': 'application/json' // Indicates the content 
-            },
-            body: JSON.stringify(todo)
-        }).then(resp => resp.json()) //console.log(result);
-        .then(data => {
-          this.isPostingTodo = false;
-          this.todos.unshift(data);
-        }) //this.fetchTodos();
-        .catch(err => this.showAlert('Failing creating Todo...'));
-      } else {
-        this.showAlert('Tittle Field is Required','warning');
-      }
-      //this.todos = this.todos.concat([this.todoTitle]);
-    },
-    removeTodo(id) {
-      //this.isLoading = true;
-      fetch('/api/todos/' + id, {
-        method: 'DELETE',
-      }).then(() => {
-        //this.isLoading = false;
-        this.todos = this.todos.filter(todo => todo.id !== id)
-      })
-      .catch(err => this.showAlert('Failing removing Todo...'));
-    },
-    updateTodo(tittle) {
-      //this.isLoading = true;
-      const { id } = this.editTodoForm.todo;
-      /* const todo = this.todos.find(
-        (todo) => todo.id === this.editTodoForm.todo.id
-      ); */
-      fetch('/api/todos/' + id, {
-        method: "PUT",
-        body: JSON.stringify({tittle}),
-        headers: {
-          "Content-Type": "application/json"
-        }
-      }).then(resp => resp.json())
-      .then(data => {
-        //this.isLoading = false;
-        const index = this.todos.findIndex(todo => todo.id === data.id);
-        this.todos[index].tittle = tittle;
-      })
-      .catch(err => this.showAlert('Failing updating Todo...'));
-      this.editTodoForm.show = false;
-    },
-    seeTodo(id) {
-      this.editTodoForm.show = true;
-      const todo = this.todos.find(todo => todo.id === id);
-      //console.log(todo);
-      this.editTodoForm.todo = {...todo}
-    }
+  });
+  const showAlert = (message, type = "danger") => {
+    alert.message = message;
+    alert.variant = type;
+    alert.show = true;
   }
-};
+  const seeTodo = (id) => {
+    editTodoForm.show = true;
+    const todo = todos.value.find(todo => todo.id === id);
+    //console.log(todo);
+    editTodoForm.todo = {...todo}
+  }
+  const fetchTodos = () => {
+    isLoading.value = true;
+    fetch('/api/todos/')
+    .then(resp => resp.json())
+    .then(data => {
+      isLoading.value = false;
+      todos.value = data;
+    })
+    .catch(err => showAlert('Failing loading Todos...' + toString(err)));
+    //console.log(todos);
+  }
+  const addTodo = (tittle) => {
+    if (tittle !== '') {
+      alert.show = false;
+      isPostingTodo.value = true;
+      const todo = { tittle };
+      fetch('/api/todos/', {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json' // Indicates the content 
+          },
+          body: JSON.stringify(todo)
+      }).then(resp => resp.json()) //console.log(result);
+      .then(data => {
+        isPostingTodo.value = false;
+        todos.value.unshift(data);
+      }) //this.fetchTodos();
+      .catch(err => showAlert('Failing creating Todo...' + toString(err)));
+    } else {
+      showAlert('Tittle Field is Required','warning');
+    }
+    //this.todos = this.todos.concat([this.todoTitle]);
+  }
+  const removeTodo = (id) => {
+    //this.isLoading = true;
+    fetch('/api/todos/' + id, {
+      method: 'DELETE',
+    }).then(() => {
+      //this.isLoading = false;
+      todos.value = todos.value.filter(todo => todo.id !== id);
+    })
+    .catch(err => showAlert('Failing removing Todo...' + toString(err)));
+  }
+  const updateTodo = (tittle) => {
+    //this.isLoading = true;
+    const { id } = editTodoForm.todo;
+    /* const todo = this.todos.find(
+      (todo) => todo.id === this.editTodoForm.todo.id
+    ); */
+    fetch('/api/todos/' + id, {
+      method: "PUT",
+      body: JSON.stringify({tittle}),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then(resp => resp.json())
+    .then(data => {
+      //this.isLoading = false;
+      const index = todos.value.findIndex(todo => todo.id === data.id);
+      todos.value[index].tittle = data.tittle;
+    })
+    .catch(err => showAlert('Failing updating Todo...' + toString(err)));
+    editTodoForm.show = false;
+  }
+  fetchTodos();
 </script>
 
 <template>
